5883d4e38b7ca409f6b558d100c7000d7bb20691
Skoushik54 <140392717+Skoushik54@users.noreply.github.com>
2024-11-27 22:01:37 +0530

    changes made

--- prime.java
+++ prime.java
@@ -2,8 +2,8 @@ class prime{
     public static void main(String[] args) {
         System.out.println("Hello World");
         int a,b;
-        a=10;
-        b=10;
+        a=15;
+        b=15;
         System.err.println(a+b);
     }
 }
